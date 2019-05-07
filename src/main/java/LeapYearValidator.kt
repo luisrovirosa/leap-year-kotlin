9cec37b038b5c2478272f7d0731ab7d426db468c
Luis Rovirosa <luisrovirosa@gmail.com>
2019-05-07 18:14:44 +0200

    refactor LeapYearValidator reduce duplicity

--- src/main/java/LeapYearValidator.kt
+++ src/main/java/LeapYearValidator.kt
@@ -1,6 +1,6 @@
 class LeapYearValidator {
     fun isLeap(number: Int): Boolean {
-        if (number % 4 == 0 && number != 100 && number != 200 && number != 300){
+        if (number % 4 == 0 && number % 100 != 0){
             return true
         }
         return false
