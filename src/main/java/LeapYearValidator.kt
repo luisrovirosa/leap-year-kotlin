d793b778776b00d2ab324254eb4b20fb3d9a7eb8
Luis Rovirosa <luisrovirosa@gmail.com>
2019-05-07 18:16:00 +0200

    refactor increase readability

--- src/main/java/LeapYearValidator.kt
+++ src/main/java/LeapYearValidator.kt
@@ -1,9 +1,14 @@
 class LeapYearValidator {
     fun isLeap(number: Int): Boolean {
-        if (number % 4 == 0 && number % 100 != 0){
+        if (isMultipleOf4(number)
+            && isNotMultipleOf100(number)){
             return true
         }
         return false
     }
+
+    private fun isNotMultipleOf100(number: Int) = number % 100 != 0
+
+    private fun isMultipleOf4(number: Int) = number % 4 == 0
 
 }
