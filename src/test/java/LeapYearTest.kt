f0f91010604c553f0ab23b0a1f02e252d8ea9129
Luis Rovirosa <luisrovirosa@gmail.com>
2019-05-07 18:09:57 +0200

    add tests for numbers 2 and 3

--- src/test/java/LeapYearTest.kt
+++ src/test/java/LeapYearTest.kt
@@ -4,7 +4,7 @@ import kotlin.test.assertFalse
 
 class LeapYearTest {
     @ParameterizedTest
-    @ValueSource(ints = [1])
+    @ValueSource(ints = [1, 2, 3])
     fun `should not be leap when is not multiple of 4`(number: Int) {
         val validator = LeapYearValidator()
 
