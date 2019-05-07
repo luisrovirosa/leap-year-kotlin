9669175e6f2d7c863ed0efeacdf5bfd1acf4e254
Luis Rovirosa <luisrovirosa@gmail.com>
2019-05-07 18:11:19 +0200

    should be leap when is multiple of 4 with 8

--- src/main/java/LeapYearValidator.kt
+++ src/main/java/LeapYearValidator.kt
@@ -1,6 +1,6 @@
 class LeapYearValidator {
     fun isLeap(number: Int): Boolean {
-        if (number == 4){
+        if (number == 4 || number == 8){
             return true
         }
         return false
