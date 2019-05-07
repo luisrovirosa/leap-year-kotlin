d3d8c772d700033bf42a698dc4d2b67dd7b594fa
Luis Rovirosa <luisrovirosa@gmail.com>
2019-05-07 18:09:15 +0200

    refactor LeapYearTest use ParametrizedTest

--- src/test/java/LeapYearTest.kt
+++ src/test/java/LeapYearTest.kt
@@ -1,12 +1,14 @@
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 import kotlin.test.assertFalse
 
 class LeapYearTest {
-    @Test
-    fun `should not be leap when is not multiple of 4`() {
+    @ParameterizedTest
+    @ValueSource(ints = [1])
+    fun `should not be leap when is not multiple of 4`(number: Int) {
         val validator = LeapYearValidator()
 
-        val isLeap = validator.isLeap(1)
+        val isLeap = validator.isLeap(number)
 
         assertFalse { isLeap }
     }
